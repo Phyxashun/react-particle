2413eedcb9bb65dcbfd0f796ea629dc1fbd2ef0e
Dustin Dew <phyxashun@gmail.com>
2026-04-05 11:58:08 -0400

    sync

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,25 @@ import Canvas from './components/Canvas/Canvas';
 import LeftPanel from './components/LeftPanel/LeftPanel';
 import Navbar from './components/Navbar/Navbar';
 import RightPanel from './components/RightPanel/RightPanel';
+import StatsDisplay from './components/Stats/StatsDisplay';
+import Title from './components/Title/Title';
 
 const App = () => (
   <div className="flex flex-col overflow-hidden">
-    <Navbar className="flex-1" />
-    <div className="flex h-screen w-full flex-row">
+    <header>
+      <Navbar>
+        <Title />
+        <StatsDisplay />
+      </Navbar>
+    </header>
+    <section className="flex h-screen w-full flex-row">
       <LeftPanel className="p-2" />
       <div className="flex flex-1 items-center justify-center">
         <Canvas />
       </div>
       <RightPanel className="p-2" />
-    </div>
+    </section>
+    <footer></footer>
   </div>
 );
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,4 @@
 import React, { type PropsWithChildren } from 'react';
-import StatsDisplay from './Stats/StatsDisplay';
-import Title from './Title';
 
 interface NavBarProps {
   className?: string;
@@ -10,17 +8,10 @@ const Navbar: React.FC<PropsWithChildren<NavBarProps>> = ({
   className = '',
   children,
 }: PropsWithChildren<NavBarProps>) => (
-  <header className={className}>
-    <nav className="navbar text-base-content bg-bg shadow-md">
-      {/* Left Side */}
-      <img className="mr-2 ml-4 size-8 flex-none" src="favicon.png" alt="logo" />
-      <Title />
-
-      {/* Right Side */}
-      <StatsDisplay />
-      {children}
-    </nav>
-  </header>
+  <nav className={`navbar text-base-content bg-bg shadow-md ${className}`}>
+    <img className="mr-2 ml-4 size-8 flex-none" src="favicon.png" alt="logo" />
+    {children}
+  </nav>
 );
 
 export default Navbar;
